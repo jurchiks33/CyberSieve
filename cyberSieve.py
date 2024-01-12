49cd9d821846d540e0352356cf422c17bf00ffdd
jurchiks33 <jurisslisans@gmail.com>
2024-01-12 16:08:02 +0100

    building background

--- cyberSieve.py
+++ cyberSieve.py
@@ -2,11 +2,17 @@ import tkinter as tk
 
 root = tk.Tk()
 
-screen_width = root.winfo_screenmmwidth()
-screen_height = root.winfo_screenmmheight()
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
 
 window_width = int(screen_width * 0.7)
 window_height = int(screen_height * 0.7)
 
 center_x = int(screen_width / 2 - window_width / 2)
 center_y = int(screen_height / 2 - window_height / 2)
+
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+root.title("Web Scraping Tool")
+
+root.mainloop()

7fbbe003a2d4990cf7a768a4833aa164995a7ad7
jurchiks33 <jurisslisans@gmail.com>
2024-01-12 16:41:35 +0100

    work on a background

--- bakground_image.py
+++ bakground_image.py
@@ -7,4 +7,8 @@ def set_background(root, image_path, bg_color='lightblue'):
         image = image.resize((200, 200), Image.ANTIALIAS)
         photo = ImageTk.PhotoImage(image)
 
-        
+        label = tk.Label(root, image=photo, bg=bg_color)
+        label.image = photo 
+        label.place(relx=0.5, rely=0.5, anchor='center')
+    except IOError:
+        print("Image cant be loaded, please check the file path.")

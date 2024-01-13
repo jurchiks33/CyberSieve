bb913962c6d042031372a1bac8ca2f21feee3c2c
jurchiks33 <jurisslisans@gmail.com>
2024-01-13 10:20:15 +0100

    label.lower on background

--- bakground_image.py
+++ bakground_image.py
@@ -6,7 +6,6 @@ def set_background(root, image_path, bg_color='lightblue'):
     label = tk.Label(root, bg=bg_color)
     label.place(x=0, y=0, relwidth=1, relheight=1)
     label.photo = None
-    label.place(x=0, y=0, relwidth=1, relheight=1)
 
     def resize_image(event):
         nonlocal label  # Use the label defined in the outer scope
@@ -15,4 +14,8 @@ def set_background(root, image_path, bg_color='lightblue'):
         label.photo = ImageTk.PhotoImage(new_image)
         label.config(image=label.photo)
 
+    # Bind the resize_image function to the window's resize event
     root.bind('<Configure>', resize_image)
+
+    # Lower the background image label to ensure it doesn't cover other widgets
+    label.lower()
